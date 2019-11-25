32b8df2f737514b6a6372bea5aa23714a6df8b65
Marko <zelenbaba.andja@gmail.com>
2019-11-25 23:27:09 +0100

    initial server created

--- temp.js
+++
@@ -1,9 +0,0 @@
-var rpiDhtSensor = require('rpi-dht-sensor');
-var dht = new rpiDhtSensor.DHT11(21);
-function read () {
-  var readout = dht.read();
-    console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
-        'humidity: ' + readout.humidity.toFixed(2) + '%');
-    setTimeout(read, 5000);
-}
-read();
